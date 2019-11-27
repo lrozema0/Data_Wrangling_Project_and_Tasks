ed46b74e9af533eed663d3fed9f6c3e3bf7bb441
lrozema0 <56605761+lrozema0@users.noreply.github.com>
2019-11-27 13:14:01 -0500

    Add files via upload

---
+++ final_project/lrozema_Final_QBS181.sql
@@ -0,0 +1,36 @@
+/* Looking at the structure of each table */
+select * from Demographics;
+select * from Conditions;
+select * from TextMessages;
+
+/* Joining the tables into a new one for easier manipulation */
+select A.*, B.*, C.* into lrozema.democontext
+from Demographics A
+inner join
+Conditions B
+on A.contactid = B.tri_patientid
+inner join 
+TextMessages C
+on A.contactid = C.tri_contactId;
+
+select top 10 * from lrozema.democontext
+
+/* There are still some duplicate where there were more than two texts sent on the latest date, so I'm only selecting one here */
+select *
+from (
+   select *,
+          row_number() over (partition by contactid order by TextSentDate desc) as row_number
+   from lrozema.democontext
+   ) as rows
+where row_number = 1
+/*This gives 3416 rows, the same as is seen in R, now I'll grab 10 rows of it*/
+
+select top 10 *
+from (
+   select *,
+          row_number() over (partition by contactid order by TextSentDate desc) as row_number
+   from lrozema.democontext
+   ) as rows
+where row_number = 1
+
+
